02189de07978004c847b2e8cf775b2a49557a02c
David Schaefer <david.schaefe@gmail.com>
2017-03-15 11:35:44 +0100

    moved data type declarations int mo_netcdf

--- mo_netcdf.f90
+++ mo_netcdf.f90
@@ -22,7 +22,8 @@ module mo_netcdf
   ! -------
   ! GNU Lesser General Public License http://www.gnu.org/licenses/
 
-  use mo_kind,         only: i8, i16, i32, f32, f64
+  use, intrinsic :: iso_c_binding, only: c_short, c_int, c_long_long, c_float, c_double
+       
   use netcdf,          only: &
        nf90_open, nf90_close, nf90_strerror, nf90_def_dim, nf90_def_var,   &
        nf90_put_var, nf90_get_var, nf90_put_att, nf90_get_att,             &
@@ -40,6 +41,14 @@ module mo_netcdf
   implicit none
 
   ! --------------------------------------------------------------------------------------
+  integer, parameter :: i8  = SELECTED_INT_KIND(2)
+  integer, parameter :: i16 = c_short
+  integer, parameter :: i32 = c_int
+  integer, parameter :: i64 = c_long_long
+  integer, parameter :: f32 = c_float
+  integer, parameter :: f64 = c_double
+
+
   type, abstract :: NcBase
 
      integer(i32) :: id
