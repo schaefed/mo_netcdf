5542d6d59ffa5c82ff4be60e5a3212a4a1c9e9ea
David Schaefer <david.schaefe@gmail.com>
2017-03-27 11:27:00 +0200

    new testing module

---
+++ test/mo_testing.f90
@@ -0,0 +1,40 @@
+module mo_testing
+
+  use mo_string
+
+  implicit none
+
+  interface assertEqual
+     module procedure assertEqualScalarsI32
+  end interface assertEqual
+
+contains
+
+  subroutine exitOut(msg)
+    character(len=*), intent(in) :: msg
+
+    print*, msg
+    stop 1
+  end subroutine exitOut
+
+  subroutine assertEqualScalarsI32(first, second, msg)
+    integer,     intent(in)           :: first, second
+    character(len=*), intent(in), optional :: msg
+    type(String) :: tmp
+
+    if (first /= second) then
+
+       if (present(msg)) then
+          tmp = String(msg)
+          print*, tmp%get()
+       end if
+
+       tmp = String("AssertionError: Values are not equal: ") &
+            + String(first) + " - " + String(second)
+
+       call exitOut(tmp%get())
+    end if
+  end subroutine assertEqualScalarsI32
+
+end module mo_testing
+  
